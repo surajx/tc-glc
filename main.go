c2e6403388f41c7f0c2ffb314335a14a427ea1ac
Suraj Narayanan S <surajx@gmail.com>
2015-06-03 16:06:44 +0530

    Adding links to challenges as comment

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
 	"github.com/surajx/tc-glc/stage2"
 )
 
+/*
+Stage-1
+1:http://community.topcoder.com/tc?module=ProjectDetail&pj=30045937 (Completed)
+2:http://community.topcoder.com/tc?module=ProjectDetail&pj=30045938 (Completed)
+3:http://community.topcoder.com/tc?module=ProjectDetail&pj=30045939 (Pending)
+4:http://community.topcoder.com/tc?module=ProjectDetail&pj=30046010 (Pending)
+Stage-2
+1:http://community.topcoder.com/tc?module=ProjectDetail&pj=30046011 (Pending)
+2:http://community.topcoder.com/tc?module=ProjectDetail&pj=30046225 (Pending)
+3:http://community.topcoder.com/tc?module=ProjectDetail&pj=30046224 (Pending)
+*/
 func main() {
 	var stage int
 	flag.IntVar(&stage, "stage", -1, "Stage to execute")
